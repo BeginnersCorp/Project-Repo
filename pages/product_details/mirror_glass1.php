8e63ab8cc8fe3b96eceeb8b1c886cc86fc32a63b
TomaRpsc <momtaz.begum09@gmail.com>
2017-12-25 20:38:19 +0600

    con

---
+++ pages/product_details/mirror_glass1.php
@@ -0,0 +1,55 @@
+<html>
+	<head>
+		<title>
+			
+		</title>
+	<head>
+	
+	<body>
+	   	<script>
+		function msg()
+		{
+			alert("Product added to cart!");
+		}
+		</script>
+			<table border=5 height="100%">
+				<tr width="100%">
+					<td>
+						<img src="../../resources/products/mirror_glass1.jpg" alt="../../resources/products/mirror_belt1.jpg" height="500" width="400" >
+					</td>
+					<td width="100%" valign="top" align="center">
+						<h1><u><b>Product name : Kool Vue Mirror Glass </b></u><h1> <br><br>
+						<h3>Details : <br><br/> Flat Type ; With backer plate ; A high quality ; OE replacement mirror glass<h3><br>
+						<table border=2 width="30%" height="30%" cellpadding="10">
+						    <tr>
+							<td>Location:</td>
+							<td>Driver Side</td>
+							</tr>
+							
+							<td>Product fit:</td>
+							<td>Direct fit</td>
+							</tr>
+							<tr>
+							<td>Heated:</td>
+							<td>Non Heated</td>
+							</tr>
+							<tr>
+							<td>Glass Type:</td>
+							<td>Flat</td>
+							</tr>
+							<tr>
+							<td>Supported:</td>
+							<td>Any cars</td>
+							</tr>
+							<tr>
+							<td>Price:</td>
+							<td>850.00</td>
+							</tr>
+						</table><br/>
+						
+						<input type="button" onclick="msg()" value="Add to cart">
+					</td>
+				</tr>
+			</table>
+	</body>
+</html>	
