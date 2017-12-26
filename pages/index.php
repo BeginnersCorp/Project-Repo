baab18d49f91bccbb3637ba86c1d90e86b6bd971
MD. Ryhan Hossain <m.ryhanhossain@gmail.com>
2017-12-26 16:26:04 +0600

    update

--- pages/index.php
+++ pages/index.php
@@ -1,33 +1,36 @@
 <html>
-<table width="100%" align="center" cellspacing="0" cellpadding="10" border="1">
+<table width="100%" height="100%" align="center" cellspacing="0" cellpadding="10" border="1">
     <tr>
         <td valign="middle" height="70">
-            <table width="100%">
+            <table width="100%" height="100%" align="center" cellspacing="0" cellpadding="10" border="0">
                 <tr>
                     <td>
                         <a href="home.php" target="contentFrame">
-                            <img height="140" width="800" src="../resources/images/x.jpg">
-                        </a>
-						<!--<a href="home.php" target="contentFrame">
-                            <img height="140" width="250" src="../resources/images/y1.jpg">
-                        </a>-->
+							<abbr title="AutoMart">
+								<img height="140" width="800" src="../resources/images/x.jpg">
+							</abbr>
+						</a>
                     </td>
-				<!--</tr>
-				<tr>-->
-                    <td align="right">
-                        <a href="home.php" target="contentFrame">Home</a>&nbsp;|
-                        <a href="login.php" target="contentFrame">Login</a>&nbsp;|
-                        <a href="registration.php" target="contentFrame">Registration</a>&nbsp;|
-						<a href="cart.php" target="contentFrame">Cart</a>
+                    <td align="left">
+                        <a href="home.php" target="contentFrame">
+							<abbr title="HOME">
+								<img height="140" width="100" src="../resources/images/home.png">
+							</abbr>
+						</a>
+                        <a href="cart.php" target="contentFrame">
+							<abbr title="CART">
+								<img height="120" width="100" src="../resources/images/cart.png">
+							</abbr>
+						</a>
                     </td>
                 </tr>
             </table>
         </td>
     </tr>
-    <tr>
-        <td align="center">
-            <br />
-            <iframe name="contentFrame" frameborder="0" width="100%" height="475" src="home.php"></iframe>
+    <tr width="100%" height="100%">
+        <td align="center" width="100%" height="100%">
+            
+            <iframe name="contentFrame" frameborder="0" width="100%" height="100%" src="home.php"></iframe>
         </td>
     </tr>
     <tr>
