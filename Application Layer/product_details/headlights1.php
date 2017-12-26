6f98432e5b039adc6794c761db1bb4729f677dfd
SakibZubaer001 <33634549+SakibZubaer001@users.noreply.github.com>
2017-12-26 20:19:51 +0600

    Image resource

--- Application Layer/product_details/headlights1.php
+++ Application Layer/product_details/headlights1.php
@@ -15,7 +15,7 @@
 			<table border=5 height="100%">
 				<tr width="100%">
 					<td>
-						<img src="../../resources/products/headlights1.jpg" alt="../../resources/products/headlights1.jpg" height="500" width="400" >
+						<img src="../../Application Layer/resources/products/headlights1.jpg" alt="../../resources/products/headlights1.jpg" height="500" width="400" >
 					</td>
 					<td width="100%" valign="top" align="center">
 						<h1><u><b>Product name : Sedan Head Lights</b></u><h1> <br><br>
