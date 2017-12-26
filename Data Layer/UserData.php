f396202792303ef3aacf21571617654d97838cc8
SakibZubaer001 <33634549+SakibZubaer001@users.noreply.github.com>
2017-12-27 04:42:33 +0600

    User database operations

--- Data Layer/UserData.php
+++ Data Layer/UserData.php
@@ -2,9 +2,63 @@
 
 	function CreateUser($user)
 	{
-		$query ;
+		$query = "INSERT INTO user(email, name, password) VALUES ('$user[email]', '$user[name]', '$user[password]')" ;
+		$resultset = ExecuteQuery($query) ;
+		return $resultset ;
+		
 	}
 
-
+	function UpdateUser($user)
+	{
+		$query = "UPDATE user SET email='$user[email]', name='$user[name]' WHERE id='$user[id]'" ;
+		$resultset = ExecuteQuery($query) ;
+		return $resultset ;
+	}
+	
+	function ChangePassword($user)
+	{
+		$query = "UPDATE user SET password='$user[password]' WHERE id='$user[id]'" ;
+		$resultset = ExecuteQuery($query) ;
+		return $resultset ;
+	}
+	
+	function DeleteUser($user)
+	{
+		$query = "DELETE FROM user WHERE id='$user[id]'" ;
+		$resultset = ExecuteQuery($query) ;
+		return $resultset ;
+	}
+	
+	function GetAllUsers()
+	{
+		$users = array() ;
+		$query = "SELECT * FROM user" ;
+		$resultset = ExecuteQuery($query) ;
+		for($i=0 ; $row=mysqli_fetch_assoc($resultset) ; ++$i)
+		{
+			$users[$i] = $row ;
+		}
+		return $users ;
+	}
+	
+	function SearchByName($name)
+	{
+		$query = "SELECT * FROM user WHERE Name LIKE '%$name%'";
+        $resultset = ExecuteQuery($query);
+        
+        $members = array();
+        for($i=0; $row = mysqli_fetch_assoc($resultset); ++$i){
+            $users[$i] = $row;
+        }
+        return $users;
+	}
+	
+	function SearchByEmail($email)
+	{
+		$query = "SELECT * FROM user WHERE email='$user[email]'";
+        $resultset = ExecuteQuery($query);
+		$user = mysqli_fetch_assoc($resultset);
+		return $user ;
+	}
 
 ?>
