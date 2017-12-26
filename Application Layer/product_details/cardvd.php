b868025f7406e8ec920896f668d99cb0b60ceaa7
SakibZubaer001 <33634549+SakibZubaer001@users.noreply.github.com>
2017-12-26 20:17:13 +0600

    resource changed

--- Application Layer/product_details/cardvd.php
+++ Application Layer/product_details/cardvd.php
@@ -15,7 +15,7 @@
 			<table border=5 height="100%">
 				<tr width="100%">
 					<td>
-						<img src="../../resources/products/cardvd.jpg" alt="../../resources/products/cardvd.jpg" height="500" width="400" >
+						<img src="../../Application Layer/resources/products/cardvd.jpg" alt="../../resources/products/cardvd.jpg" height="500" width="400" >
 					</td>
 					<td width="100%" valign="top" align="center">
 						<h1><u><b>Product name : Car DVD Player</b></u><h1> <br><br>
