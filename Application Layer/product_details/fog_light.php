c1d8e9d8713089a9b36d265d308e5dad8b76a026
SakibZubaer001 <33634549+SakibZubaer001@users.noreply.github.com>
2017-12-26 20:18:59 +0600

    resource - new

--- Application Layer/product_details/fog_light.php
+++ Application Layer/product_details/fog_light.php
@@ -15,7 +15,7 @@
 			<table border=5 height="100%">
 				<tr width="100%">
 					<td>
-						<img src="../../resources/products/fog_light.jpg" alt="../../resources/products/fog_light.jpg" height="500" width="400" >
+						<img src="../../Application Layer/resources/products/fog_light.jpg" alt="../../resources/products/fog_light.jpg" height="500" width="400" >
 					</td>
 					<td width="100%" valign="top" align="center">
 						<h1><u><b>Product name : Fog Light</b></u><h1> <br><br>
