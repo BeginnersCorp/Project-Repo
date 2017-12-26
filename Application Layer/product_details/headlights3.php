b6e9af37998e7526dfa73c3ffc52a9956b315b4b
SakibZubaer001 <33634549+SakibZubaer001@users.noreply.github.com>
2017-12-26 20:20:52 +0600

    product image fix

--- Application Layer/product_details/headlights3.php
+++ Application Layer/product_details/headlights3.php
@@ -15,7 +15,7 @@
 			<table border=5 height="100%">
 				<tr width="100%">
 					<td>
-						<img src="../../resources/products/headlights3.jpg" alt="../../resources/products/headlights3.jpg" height="500" width="400" >
+						<img src="../../Application Layer/resources/products/headlights3.jpg" alt="../../resources/products/headlights3.jpg" height="500" width="400" >
 					</td>
 					<td width="100%" valign="top" align="center">
 						<h1><u><b>Product name : SUV Head Lights</b></u><h1> <br><br>
