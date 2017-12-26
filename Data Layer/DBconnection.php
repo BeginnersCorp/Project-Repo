ee7621d216d6ad0ded2bc5d2b16fca1ba1de0a2d
SakibZubaer001 <33634549+SakibZubaer001@users.noreply.github.com>
2017-12-27 00:44:39 +0600

    Database connection

--- Data Layer/DBconnection.php
+++ Data Layer/DBconnection.php
@@ -4,5 +4,14 @@
     $password="";
     $dbname="ecommerce";
     $port=3306;
+	
+	function ExecuteQuery($query)
+	{
+		global $server, $username, $password, $dbname, $port ;
+		$connection = mysqli_connect($server, $username, $password, $dbname, $port) ;
+		$resultset = mysqli_query($connection, $sql) ;
+		mysqli_close($connection) ;
+		return $resultset ;
+	}
 
 ?>
