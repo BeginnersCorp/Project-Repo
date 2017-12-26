19f1a89732953eb27b670385e4f3d4879263c515
MD. Ryhan Hossain <m.ryhanhossain@gmail.com>
2017-12-27 01:23:10 +0600

    update

--- Application Layer/contact.php
+++ Application Layer/contact.php
@@ -22,9 +22,29 @@
             </table>
         </td>
     </tr>
-    <tr width="100%" height='1000'>
-        <td>
-            <iframe name="contentFrame" frameborder="0" width="100%" height="100%" src="home.php"></iframe>
+    <tr>
+        <td valign="middle">
+			<table width="100%" height="100%" align="center" cellspacing="0" cellpadding="10" border="0">
+				<tr><td>
+					<table width="100%" height="100%" align="center" cellspacing="0" cellpadding="10" border="0">
+						<tr>
+							<td align="middle">
+							<h2>
+								Mirpur Shoping Complex<br/>
+								Mirpur-2,Dhaka<br/>
+								+021234567<br/>
+								info@automart.com
+							</h2>
+							</td>
+						</tr>
+					</table>
+				</td></tr>
+				<tr>
+					<td>
+						<img height="100%" width="100%" src="resources/images/map.png">
+					</td>
+				</tr>
+			</table>
         </td>
     </tr>
     <tr>
