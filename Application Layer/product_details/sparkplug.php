ec8961a13f5b496fa780d56a4694786bbd93075b
SakibZubaer001 <33634549+SakibZubaer001@users.noreply.github.com>
2017-12-26 20:25:28 +0600

    Image resource modified

--- Application Layer/product_details/sparkplug.php
+++ Application Layer/product_details/sparkplug.php
@@ -15,7 +15,7 @@
 			<table border=5 height="100%">
 				<tr width="100%">
 					<td>
-						<img src="../../resources/products/sparkplug.jpg" alt="../../resources/products/sparkplug.jpg" height="500" width="400" >
+						<img src="../../Application Layer/resources/products/sparkplug.jpg" alt="../../resources/products/sparkplug.jpg" height="500" width="400" >
 					</td>
 					<td width="100%" valign="top" align="center">
 						<h1><u><b>Product name : Car Engine SPARKPLUG</b></u><h1> <br><br>
