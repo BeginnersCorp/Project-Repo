d66e93fd16dad2af8fda11a47d6bdd479ace6ded
SakibZubaer001 <33634549+SakibZubaer001@users.noreply.github.com>
2017-12-26 20:14:03 +0600

    resource modifications

--- Application Layer/product_details/bumper2.php
+++ Application Layer/product_details/bumper2.php
@@ -15,7 +15,7 @@
 			<table border=5 height="100%">
 				<tr width="100%">
 					<td>
-						<img src="../../resources/products/bumper2.jpeg" alt="../../resources/products/bumper2.jpeg" height="500" width="400" >
+						<img src="../../Application Layer/resources/products/bumper2.jpeg" alt="../../resources/products/bumper2.jpeg" height="500" width="400" >
 					</td>
 					<td width="100%" valign="top" align="center">
 						<h1><u><b>Product name : Sedan Bumpers (Front + Back + Side)</b></u><h1> <br><br>
