3390ae423c5b2247a83c26fe63c6defc60c8edf1
SakibZubaer001 <33634549+SakibZubaer001@users.noreply.github.com>
2017-12-27 01:40:12 +0600

    Cart Fixed

--- Application Layer/cart.php
+++ Application Layer/cart.php
@@ -11,21 +11,21 @@
         <th colspan="3"></th>
     </tr>
     <tr>
-        <td align="center" width="80"><a> <img src="../resources/products/red_sports_seat.jpg"  width="80" height="100"> </a></td>
+        <td align="center" width="80"><a> <img src="../Application Layer/resources/products/red_sports_seat.jpg"  width="80" height="100"> </a></td>
         <td align="center">Red Sports Seat 1</td>
         <td align="center">25000</td>
 		<td align="center">1</td>
 		<td align="center"><a href="">REMOVE</a></td>
     </tr>
     <tr>
-        <td align="center" width="80"><a> <img src="../resources/products/red_sports_seat1.jpg"  width="80" height="100"> </a></td>
+        <td align="center" width="80"><a> <img src="../Application Layer/resources/products/red_sports_seat1.jpg"  width="80" height="100"> </a></td>
         <td align="center">Red Sports Seat 2</td>
         <td align="center">28000</td>
 		<td align="center">1</td>
 		<td align="center"><a href="">REMOVE</a></td>
     </tr>
     <tr>
-        <td align="center" width="80"><a> <img src="../resources/products/stearing_wheel.jpg"  width="80" height="100"> </a></td>
+        <td align="center" width="80"><a> <img src="../Application Layer/resources/products/stearing_wheel.jpg"  width="80" height="100"> </a></td>
         <td align="center">Stearing Wheel</td>
         <td align="center">15000</td>
 		<td align="center">1</td>
