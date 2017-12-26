1261cff08e49fa68cf8d192216321bbce8a7fe25
SakibZubaer001 <33634549+SakibZubaer001@users.noreply.github.com>
2017-12-26 20:19:30 +0600

    image resources

--- Application Layer/product_details/gearbox.php
+++ Application Layer/product_details/gearbox.php
@@ -15,7 +15,7 @@
 			<table border=5 height="100%">
 				<tr width="100%">
 					<td>
-						<img src="../../resources/products/gearbox.jpg" alt="../../resources/products/gearbox.jpg" height="500" width="400" >
+						<img src="../../Application Layer/resources/products/gearbox.jpg" alt="../../resources/products/gearbox.jpg" height="500" width="400" >
 					</td>
 					<td width="100%" valign="top" align="center">
 						<h1><u><b>Product name : Car Engine GEARBOX</b></u><h1> <br><br>
