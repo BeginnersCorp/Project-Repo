fa76b7b2f39a17fc1d326df8ce4aaa5001e2fd91
Samiha Jaman <33634694+SamihaJaman21@users.noreply.github.com>
2017-12-28 05:20:05 +0600

    Update thank_you.php

--- Application Layer/thank_you.php
+++ Application Layer/thank_you.php
@@ -1,5 +1,20 @@
 <html>
-<fieldset><h1>Thank You for buying from us</h1></fieldset>
-<br />
-<fieldset>
+
+	<table width="100%" height="100%" align="center" cellspacing="0" cellpadding="10" border="0">
+		<tr><td>
+			<table width="100%" height="100%" align="center" cellspacing="0" cellpadding="10" border="0">
+				<tr>
+					<td align="right">
+					<a href="login.php" target="contentFrame">Login</a>&nbsp;|
+					<a href="home.php" target="contentFrame">Home</a>
+				    </td>
+					<fieldset>
+						<h1>Thank You for buying from us</h1>
+					</fieldset>
+					<br />
+					</tr>
+			</table>
+		</td></tr>
+	</table>
+	
 </html>
