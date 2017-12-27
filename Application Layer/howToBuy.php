eb5048de0f37d09aa388120926dcedb5420852df
Samiha Jaman <33634694+SamihaJaman21@users.noreply.github.com>
2017-12-28 05:36:19 +0600

    Add files via upload

---
+++ Application Layer/howToBuy.php
@@ -0,0 +1,77 @@
+<html>
+	<head>
+		<title>How To Buy</title>
+	</head>
+	<body>
+		<table border="1" width="100%">
+			ef="logIn.php">Log In</a></td>
+									</tr>
+									
+								</table>
+							</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3" align="center">
+					<table>
+						<tr>
+							<td>
+								<table border="1" height="800">
+
+								
+										</td>
+									</tr>
+								</table>
+								
+							</td>
+							<td width="800">
+								
+								<h2>How to Buy</h2>
+								<h3>Pay Cash on Delivery after receiving your product at your home!</h3>
+								<hr/>
+								
+								<p align="justify">
+									
+
+								Get your product delivered at your home and then pay cash to our delivery "person. No advance payment is required.
+								</p>
+								<h3>Enjoy the easy payment option of bKash!</h3>
+								
+								<p align="justify">
+									
+
+								Payment process
+								
+									<ul>
+										<li>Go to bKash Mobile Menu by dialing *247#
+										<li>Choose 'Payment
+										<li>Enter the Account Number; +021234567
+										<li>Enter Bill Amount
+										<li>Enter a reference against your payment (Your Name/Phone Number)
+										<li>Enter the counter number: 1
+										<li>Now enter your bKash PIN to confirm Done!
+										<li>You will receive a confirmation message from bKash
+										<li>Please, bKash pay to +021234567
+									</ul>
+
+								</p>
+								<img src="resources/b.jpg" width="800"></img>
+								
+								
+							</td>
+						</tr>
+						
+					</table>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3
+				</td>
+			</tr>
+		
+		</table>
+	</body>
+
+</html>
