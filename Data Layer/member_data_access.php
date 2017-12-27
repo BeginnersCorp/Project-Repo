ba5b8117d9b26cb59d598729204b78885a0594f0
SakibZubaer001 <33634549+SakibZubaer001@users.noreply.github.com>
2017-12-27 22:57:30 +0600

    Database error fixed

--- Data Layer/member_data_access.php
+++ Data Layer/member_data_access.php
@@ -2,14 +2,14 @@
 <?php
 	function addMemberToDB($member)
 	{
-        $sql = "INSERT INTO member(Member_ID, Password, Name, Email, Type, Status) VALUES ('$member[Member_ID]', '$member[Password]', '$member[Name]', '$member[Email]', '$member[Type]', '$member[Status]')";
+        $sql = "INSERT INTO member(Member_ID, Name, Email, Password) VALUES ('$member[Member_ID]', '$member[Name]', '$member[Email]', '$member[Password]')" ;
         $result = executeSQL($sql);
         return $result;
     }
 
 	function editMemberToDB($member)
 	{
-        $sql = "UPDATE member SET Password='$member[Password]',Name='$member[Name]',Email='$member[Email]',Type='$member[Type]',Status='$member[Status]' WHERE Member_ID='$member[Member_ID]'";
+        $sql = "UPDATE member SET Name='$member[Name]',Email='$member[Email]', Password='$member[Password]' WHERE Member_ID='$member[Member_ID]'";
         $result = executeSQL($sql);
         return $result;
     }
@@ -99,32 +99,6 @@
         $member = mysqli_fetch_assoc($result);
         
         return $member;
-    }
-	
-	// function getMembersByTypeFromDB($Type)
-	// {
-        // $sql = "SELECT * FROM member WHERE Type=$Type";
-        // $result = executeSQL($sql);
-        
-        // $members = array();
-        // for($i=0; $row = mysqli_fetch_assoc($result); ++$i){
-            // $products[$i] = $row;
-        // }
-        
-        // return $members;
-    // }
-	
-	function getMembersByTypeFromDB($Status)
-	{
-        $sql = "SELECT * FROM member WHERE Status=$Status";
-        $result = executeSQL($sql);
-        
-        $members = array();
-        for($i=0; $row = mysqli_fetch_assoc($result); ++$i){
-            $products[$i] = $row;
-        }
-        
-        return $members;
     }
 ?>
 	
