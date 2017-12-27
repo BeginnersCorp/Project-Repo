35dcfc7b1e94bdb29aabd1ba0244fdce794f414c
MD. Ryhan Hossain <m.ryhanhossain@gmail.com>
2017-12-28 00:07:52 +0600

    u

--- Application Layer/about.php
+++ Application Layer/about.php
@@ -1,27 +1,5 @@
 <html>
 <table width="100%" height="100%" align="center" cellspacing="0" cellpadding="10" border="1">
-    <tr>
-        <td valign="middle" height="70">
-            <table width="100%" height="100%" align="center" cellspacing="0" cellpadding="10" border="0">
-                <tr>
-                    <td>
-                        <a href="../index.php" target="contentFrame">
-							<abbr title="AutoMart">
-								<img height="140" width="800" src="resources/images/x.jpg">
-							</abbr>
-						</a>
-                    </td>
-                    <td align="left">
-                        <a href="../index.php" target="contentFrame">
-							<abbr title="HOME">
-								<img height="140" width="100" src="resources/images/home.png">
-							</abbr>
-						</a>
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
     <tr width="100%" height='100'>
         <td>
 		    <h2>About AutoMart<h2>
@@ -62,29 +40,6 @@
 	    <td>
 	       <h2>Whatever your needed auto parts and accessories are,we're the AutoMart here to help you.</h2>
         </td>
-    </tr>
-	
-	<tr>
-		<td valign="middle">
-			<table width="100%" align="center" cellspacing="0" cellpadding="10" border="0">
-				<tr>
-					<td align="middle">
-						<h2>Information<h2><br/>
-						<a href="about.php">About Us</a><br/>
-						<a href="contact.php">Contact Us</a>
-					</td>
-					
-					<td align="center">
-						Copyright &copy; 2017<br/>All rights reserved by AutoMart.com
-					</td>
-					
-					<td align="middle">
-						<h2>Our Location</h2><br/>
-						Mirpur Shoping Complex<br/>Mirpur-2,Dhaka
-					</td>
-				</tr>
-			</table>
-		</td>
     </tr>
 </table>
 </html>
