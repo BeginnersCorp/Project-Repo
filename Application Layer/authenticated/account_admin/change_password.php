0098f4d295c977cab1d614235414dbf0e7b3cfb3
MD. Ryhan Hossain <m.ryhanhossain@gmail.com>
2017-12-28 14:13:47 +0600

    Update change_password.php

--- Application Layer/authenticated/account_admin/change_password.php
+++ Application Layer/authenticated/account_admin/change_password.php
@@ -1,10 +1,36 @@
 <html>
+
+<?php
+	
+	$server="localhost";
+	$database="automart";
+	$conn = mysqli_connect($server, "root", "", $database);
+	
+	session_start();
+	if(isset($_SESSION['uname'])){
+		$username = $_SESSION['uname'];
+	}
+
+	if(isset($_POST['butto'])){
+			$curpass = $_POST['cpass'];
+			$newpass = $_POST['npass'];
+			$repass = $_POST['rpass'];
+			
+			$sql = "UPDATE member SET Password='$repass' WHERE Email='$username' ";
+			
+			if ($conn->query($sql) === TRUE){
+				echo 'UPDATED';
+			}
+	}
+$conn->close();
+
+?>
 <fieldset>This is message box</fieldset>
 <br />
 <fieldset>
     <legend><b>CHANGE PASSWORD</b></legend>
     <br/>
-    <form>
+    <form action="" method="post">
         <table width="100%" cellpadding="0" cellspacing="0">
             <tr>
                 <td width="150"></td>
@@ -15,26 +41,26 @@
             <tr>
                 <td><font size="3">Current Password</font></td>
 				<td>:</td>
-                <td><input type="password" /></td>
+                <td><input type="password" name="cpass"/></td>
                 <td></td>
             </tr>
             <tr><td colspan="4"><hr/></td></tr>
             <tr>
                 <td><font size="3" color="green">New Password</font></td>
 				<td>:</td>
-                <td><input type="password" /></td>
+                <td><input type="password" name="npass" /></td>
                 <td></td>
             </tr>
             <tr><td colspan="4"><hr/></td></tr>
             <tr>
                 <td><font size="3" color="red">Retype New Password</font></td>
 				<td>:</td>
-                <td><input type="password" /></td>
+                <td><input type="password" name="rpass"/></td>
                 <td></td>
             </tr>
         </table>
         <hr />
-        <input type="submit" value="Update" />
+        <button type="submit" name="butto">Save</button>
         <a href="dashboard.php">Dashboard</a>
     </form>
 </fieldset>
