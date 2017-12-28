3f197bfbabed1b462630b4f07011e33993bc6a33
MD. Ryhan Hossain <m.ryhanhossain@gmail.com>
2017-12-28 18:58:26 +0600

    u

---
+++ automart.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 28, 2017 at 12:18 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `automart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice`
+--
+
+CREATE TABLE `invoice` (
+  `Invoice_Code` varchar(8) NOT NULL,
+  `Member_ID` varchar(8) NOT NULL,
+  `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Status` varchar(15) NOT NULL DEFAULT 'Pending',
+  `Payment_Status` varchar(10) NOT NULL DEFAULT 'Not Paid',
+  `Payment_Method` varchar(100) NOT NULL,
+  `Shipping_Address` varchar(10000) NOT NULL,
+  `Billing_Address` varchar(10000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `Member_ID` varchar(8) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  `Name` varchar(40) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `Type` int(1) NOT NULL,
+  `Status` varchar(10) NOT NULL DEFAULT 'Active',
+  `Member_Since` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Last_Logged_in` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Total_Purchase` int(8) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`Member_ID`, `Password`, `Name`, `Email`, `Type`, `Status`, `Member_Since`, `Last_Logged_in`, `Total_Purchase`) VALUES
+('1', '123456', 'Ryhan Hosain', 'ryhan@gmail.com', 1, 'Active', '2017-12-27 22:54:56', '2017-12-27 22:54:56', 0),
+('2', '123456', 'ryhan sohan', 'ry@mail.com', 0, 'Active', '2017-12-27 22:59:37', '2017-12-27 22:59:37', 0),
+('3', '123456', 'r hossain', 'ray@mail.com', 1, 'Active', '2017-12-27 22:59:37', '2017-12-27 22:59:37', 0),
+('4', '123654', 'R Hossain', 'rh@mail.com', 0, '', '2017-12-27 23:14:08', '2017-12-27 23:14:08', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_product`
+--
+
+CREATE TABLE `order_product` (
+  `Order_Code` varchar(8) NOT NULL,
+  `Product_Code` varchar(8) NOT NULL,
+  `Quantity` int(8) NOT NULL,
+  `Invoice_Code` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `Product _Code` varchar(8) NOT NULL,
+  `Name` varchar(1000) NOT NULL,
+  `Quantity` int(8) NOT NULL,
+  `Total_Sells` int(8) NOT NULL DEFAULT '0',
+  `Price` float NOT NULL,
+  `Last_Sold` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `Catagory` varchar(30) NOT NULL,
+  `Brand` varchar(30) NOT NULL,
+  `Size` varchar(10) NOT NULL,
+  `Description` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
