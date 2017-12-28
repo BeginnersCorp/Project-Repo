f29ba3450a040f8d1fea25b754eb1a8d3fb1521f
Samiha Jaman <33634694+SamihaJaman21@users.noreply.github.com>
2017-12-28 06:31:03 +0600

    Update registration.php

--- Application Layer/registration.php
+++ Application Layer/registration.php
@@ -21,7 +21,7 @@
     if($_SERVER['REQUEST_METHOD']=="POST")
 	{
 		$name=trim($_POST['name']);
-        $email=trim($_POST['email']);
+         $email=trim($_POST['email']);
 		$password=trim($_POST['password']);
 		$cpassword=trim($_POST['cpassword']);
 		
